81be356655710ddb0951390c918886d6ccfe330a
Max <max.09@outlook.com>
2019-01-08 20:13:19 +0100

    belgian nes

--- API_call.php
+++ API_call.php
@@ -3,7 +3,7 @@
         $ch = curl_init(); 
 
         // set url 
-        curl_setopt($ch, CURLOPT_URL, "https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=fc48327c5dd6468c9c80f74d205727c7"); 
+        curl_setopt($ch, CURLOPT_URL, "https://newsapi.org/v2/top-headlines?country=be&apiKey=fc48327c5dd6468c9c80f74d205727c7"); 
 
         //return the transfer as a string 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
