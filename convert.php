23cc242160f7ba266ae6eb9ba7bad03c2db24d08
Max <max.09@outlook.com>
2019-01-09 19:16:18 +0100

    YESS ! finished creating html file

--- convert.php
+++ convert.php
@@ -13,6 +13,13 @@ echo $body;
 $end = "<br><br><br><em>Cet article a été généré par Web2Kindle à ".  date("h:i:sa")."</em>";
 echo $end;
 
+$htmlstring = "<!DOCTYPE html>\n <html> \n <head> <meta charset=\"UTF-8\"> \n<title>".$title."</title> \n</head> \n<body> \n <h1>".$title."</h1> \n <p>".$body.$end. "</p> \n</body>";
+
+// Create a file 
+$htmlfile = fopen("article.html", "w");
+
+// Write in file 
+fwrite($htmlfile, $htmlstring);
 
 
   ?>
