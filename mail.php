8e0378fc4f17eba7e49baa92c37706472992ed06
Max <max.09@outlook.com>
2019-01-10 18:05:01 +0100

    using PHPMailer ?

--- mail.php
+++ mail.php
@@ -5,6 +5,16 @@
 $flux = imap_open("{ssl0.ovh.net:993/imap/ssl}INBOX", "info@qonnect.fr", "fy3RBkgk78");
 
 $imap_obj = imap_check($flux);
-var_dump($imap_obj);
+
+$sucess = imap_mail("max.09@outlook.com", "test ", "THIS IS A TEST MESSAGE.");
+
+echo $sucess;
+if ($sucess) {
+	echo "Mail OK.";
+}
+else {
+	"A problem occured.";
+}
+// var_dump($imap_obj);
 
   ?>
