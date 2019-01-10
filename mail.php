c03076764978717578d938775004ba9ed47b3e47
Max <max.09@outlook.com>
2019-01-10 19:14:31 +0100

    ça marche

--- mail.php
+++ mail.php
@@ -1,20 +1,20 @@
 <?php
+    // Tentative de connexion
+$imapLink = imap_open("{ssl0.ovh.net:993/imap/ssl}INBOX", "info@qonnect.fr", "fy3RBkgk78");
 
+    // Test sur le retour de la fonction imap_open()
+    if(!$imapLink) // Échec
+    {
+    	echo "La connexion a échoué.";
+    }
+    else // Connexion établie
+    {
+    	echo "Connexion établie.";
+    }
 
+     // On récupère les informations
+    $mailBoxInfos = imap_check($imapLink);
 
-$flux = imap_open("{ssl0.ovh.net:993/imap/ssl}INBOX", "info@qonnect.fr", "fy3RBkgk78");
+    print_r($mailBoxInfos);
 
-$imap_obj = imap_check($flux);
-
-$sucess = imap_mail("max.09@outlook.com", "test ", "THIS IS A TEST MESSAGE.");
-
-echo $sucess;
-if ($sucess) {
-	echo "Mail OK.";
-}
-else {
-	"A problem occured.";
-}
-// var_dump($imap_obj);
-
-  ?>
+    ?>
