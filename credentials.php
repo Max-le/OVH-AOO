fcd4f3587aee4c3d2daeaae386a99330a68a95bb
Max-le <34814555+Max-le@users.noreply.github.com>
2019-04-14 22:51:01 +0200

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,9 +1,9 @@
 
 <?php 
 
-$appkey = '9WKB1PtG17IQGkHy'; 
-$appsecret = '8Mh7TWtlEBszINr8nblFhQy1wTtfZB4O' ; 
-$consumerkey = 'JGHsZ56VPMtqKerGx11a2yP5ipHBswOf' ;
+$appkey = 'XXXXXXXXXXXXXX'; 
+$appsecret = 'XXXXXXXXXXXXXX' ; 
+$consumerkey = 'XXXXXXXXXXXXXX' ;
 
 ?> 
 
